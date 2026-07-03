9520c1fb14129793c682508e5d5765eeb8e14133
ruskul2712 <ruskul2712@gmail.com>
2026-07-03 11:20:07 +0500

    Create first Blade view

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\HelloController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function (){
-    print "Hello";
-});
-Route::get('/my-name', function (){
-    print 'My name is Rustam';
-});
+Route::get('/post', [PostController::class, 'index']);
+
+Route::get('/my-name', [HelloController::class, 'index']);
