7f1d8c3cd9fd43c2eac602a2b748fcb899647254
ruskul2712 <ruskul2712@gmail.com>
2026-07-03 12:31:22 +0500

    Configure Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'user_id',
+        'discription'
+    ];
 }
