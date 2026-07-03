ed15c9ede9d900c98733904b6850800d98635035
ruskul2712 <ruskul2712@gmail.com>
2026-07-03 10:39:22 +0500

    commit Route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function (){
+    print "Hello";
+});
+Route::get('/my-name', function (){
+    print 'My name is Rustam';
+});
